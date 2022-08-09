779875e6c697e6265b57644165952013a19065b5
Yurii <Rapacta@gmail.com>
2022-08-09 20:36:06 +0200

    re_update

---
+++ src/components/Feedback/ButtonFeedBack/ButtonFeedBack.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import css from './ButtonFeedBack.module.css'
+
+const ButtonFeedBack = ({ onClickButtonFeedback, buttonName }) => {
+  return (
+    <div>
+      {buttonName.map(name => (
+        <button
+          type="button"
+          key={name}
+          onClick={onClickButtonFeedback}
+          name={name}
+        >
+          {name}
+        </button>
+      ))}
+    </div>
+  );
+};
+ButtonFeedBack.propTypes = {
+  onClickButtonFeedback: PropTypes.func.isRequired,
+  buttonName: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+export default ButtonFeedBack;
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useState, useEffect } from "react";
 import Notification from "./Statistics/Notofocation";
 import Statistics from './Statistics/Statistics'
-import css from './Feedback.module.css'
+// import css from './Feedback.module.css'
+import ButtonFeedBack from "./ButtonFeedBack/ButtonFeedBack";
+
+const buttonName = ['Good', 'Neutral', 'Bad'];
 
 const Feedback = () => {
     const [good, setGood] = useState(0);
@@ -9,6 +13,26 @@ const Feedback = () => {
     const [bad, setBad] = useState(0);
     const [total, setTotal] = useState(0)
     
+
+  const clickButtonFeedback = event => {
+    let name = event.target.name.toLowerCase();
+
+    switch (name) {
+      case 'good':
+        setGood(good => good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral => neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad => bad + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+
     useEffect(() => {
         setTotal(good+bad+neutral)
 }, [good, neutral, bad])
@@ -16,9 +40,7 @@ const Feedback = () => {
     return (
         <>
         <div>
-            <button className={css} type="button" onClick={() => setGood(good + 1)}>Good</button>
-            <button className={css} type="button" onClick={() => setNeutral(neutral + 1)}>neutral</button>
-            <button className={css} type="button" onClick={() => setBad(bad + 1)}>bad</button>
+                <ButtonFeedBack buttonName={buttonName} onClickButtonFeedback={clickButtonFeedback} />
         </div>
             <h2>Statistics</h2>
             {total === 0 ?
